ba782c8bfb87cad5a04673a64fba66f19588ec0e
Frederik Haagensen <frehaa@hotmail.dk>
2022-08-09 13:13:34 +0200

    Make and mute(?) sound

--- handmade/main.cpp
+++ handmade/main.cpp
@@ -1,6 +1,10 @@
 #include <windows.h>
 #include <stdint.h>
 #include <math.h>
+#include <dsound.h>
+
+#define DIRECT_SOUND_CREATE(name) HRESULT WINAPI name(LPCGUID pcGuidDevice, LPDIRECTSOUND *ppDs, LPUNKNOWN pUnkPointer)
+typedef DIRECT_SOUND_CREATE(direct_sound_create);
 
 void println(LPCSTR m) {
     OutputDebugStringA(m);
@@ -8,6 +12,8 @@ void println(LPCSTR m) {
 }
 
 typedef void(*pixel_update_fn)(uint32_t*, int, int, int);
+class AudioWrapper;
+typedef int16_t(*sample_fn)(AudioWrapper*);
 
 struct Bitmap {
     BITMAPINFO info;
@@ -15,6 +21,153 @@ struct Bitmap {
     int height;
     int width;
     int bytesPerPixel;
+};
+
+class AudioWrapper {
+public:
+    int samplesPerSecond;
+    uint32_t runningSampleIndex;
+    int bufferSeconds;
+    int bytesPerSample;
+    int bufferSize;
+    sample_fn sampler;
+    LPDIRECTSOUNDBUFFER buffer;
+
+    AudioWrapper(HWND windowHandle, int samplesPerSecond, int bytesPerSample, int bufferSeconds, sample_fn sampler) : 
+                    samplesPerSecond(samplesPerSecond), bytesPerSample(bytesPerSample), bufferSeconds(bufferSeconds), sampler(sampler) {
+        bufferSize = samplesPerSecond * bytesPerSample * bufferSeconds;
+        runningSampleIndex = 0;
+        initDirectSound(windowHandle);
+
+        // tick(0);
+        buffer->Play(0, 0, DSBPLAY_LOOPING);
+    }
+
+    AudioWrapper(HWND windowHandle, int samplesPerSecond, int bytesPerSample, sample_fn sampler) : AudioWrapper(windowHandle, samplesPerSecond, bytesPerSample, 1, sampler) {}
+    AudioWrapper(HWND windowHandle, int samplesPerSecond, sample_fn sampler) : AudioWrapper(windowHandle, samplesPerSecond, sizeof(int16_t) * 2, 1, sampler) {}
+
+    void tick(int t) {
+        DWORD playCursor;
+        DWORD writeCursor;
+        if (FAILED(buffer->GetCurrentPosition(&playCursor, &writeCursor))) {
+            println("Failed to get sound cursor positions");
+            return;
+        }
+
+        DWORD bytesToLock = (runningSampleIndex * bytesPerSample) % bufferSize;
+        DWORD bytesToWrite = calculateBytesToWrite(bytesToLock, playCursor); 
+
+        void *region1;
+        DWORD region1Size;
+        void *region2;
+        DWORD region2Size;
+        if (FAILED(buffer->Lock(bytesToLock, bytesToWrite, &region1, &region1Size, &region2, &region2Size, 0))) {
+            println("Failed to lock sound buffer");
+            return;
+        }
+
+        writeSamples(region1, region1Size);
+        writeSamples(region2, region2Size);
+
+        buffer->Unlock(region1, region1Size, region2, region2Size);
+    }
+
+private:
+    void initDirectSound(HWND windowHandle) {
+        // TODO: Do we need to dynamically load this? 
+        // * Not if we include Dsound.lib in compilation
+        HMODULE lib = LoadLibraryA("dsound.dll");
+        if (lib == nullptr) {
+            println("Failed to load library");
+            return;
+        }
+        direct_sound_create *DirectSoundCreate = (direct_sound_create*) GetProcAddress(lib, "DirectSoundCreate");
+
+        LPDIRECTSOUND directSound;
+        if (//directSoundCreate == nullptr || 
+            FAILED(DirectSoundCreate(0, &directSound, 0))) {
+            println("Failed to load library");
+            return;
+        }
+
+        if (FAILED(directSound->SetCooperativeLevel(windowHandle, DSSCL_PRIORITY))) {
+            println("Failed to set cooperative level");
+            return;
+        }
+
+        DSBUFFERDESC setupBufferDescription = {0};
+        setupBufferDescription.dwSize = sizeof(setupBufferDescription);
+        setupBufferDescription.dwFlags = DSBCAPS_PRIMARYBUFFER;
+
+        // Setup buffer is what the WINAPI refers to as the primary buffer
+        LPDIRECTSOUNDBUFFER setupSoundBuffer;
+        if (FAILED(directSound->CreateSoundBuffer(&setupBufferDescription, &setupSoundBuffer, 0))) {
+            println("Failed to create setup sound buffer");
+            return;
+        }
+
+        WAVEFORMATEX waveFormat = initWaveFormat(samplesPerSecond);
+        if (FAILED(setupSoundBuffer->SetFormat(&waveFormat))) {
+            println("Failed to set sound buffer format");
+            return;
+        }
+
+        DSBUFFERDESC bufferDescription = {0};
+        bufferDescription.dwSize = sizeof(bufferDescription);
+        bufferDescription.dwFlags = 0;
+        bufferDescription.dwBufferBytes = bufferSize;
+        bufferDescription.lpwfxFormat = &waveFormat;
+        
+        LPDIRECTSOUNDBUFFER soundBuffer;
+        if (FAILED(directSound->CreateSoundBuffer(&bufferDescription, &soundBuffer, 0))) {
+            println("Failed to create sound buffer");
+            return;
+        }
+        this->buffer = soundBuffer;
+    }
+    
+    WAVEFORMATEX initWaveFormat(int samplesPerSecond){
+        WAVEFORMATEX waveFormat = {};
+        waveFormat.wFormatTag = WAVE_FORMAT_PCM;
+        waveFormat.nChannels = 2;
+        waveFormat.nSamplesPerSec = samplesPerSecond;
+        waveFormat.wBitsPerSample = 16;
+        waveFormat.cbSize = 8;
+        waveFormat.nBlockAlign = (waveFormat.nChannels * waveFormat.wBitsPerSample) / 8;
+        waveFormat.nAvgBytesPerSec = waveFormat.nSamplesPerSec * waveFormat.nBlockAlign;
+        return waveFormat;
+    }
+
+    void writeSamples(void *region, int regionSize) {
+        int16_t *sampleOut = (int16_t *)region;
+        int sampleCount = regionSize / bytesPerSample;
+        for (int i = 0; i < sampleCount; ++i) {
+            int16_t sampleValue = sampler(this);
+            write(sampleOut, sampleValue);
+        }
+    }
+
+    int16_t sample() {
+        int16_t sampleValue = sampler(this);
+        runningSampleIndex += 1;
+        return sampleValue;
+    }
+
+    void write(int16_t *sampleOut, int16_t sampleValue) {
+        // Write and increment to pointers
+        *sampleOut++ = sampleValue; 
+        *sampleOut++ = sampleValue;
+    }
+
+    int calculateBytesToWrite(DWORD bytesToLock, DWORD playCursor) {
+        if (bytesToLock == playCursor) {
+            return bufferSize;
+        } else if (bytesToLock > playCursor) {
+            return playCursor + bufferSize - bytesToLock;
+        } else {
+            return playCursor - bytesToLock;
+        }
+    }
 };
 
 bool globalIsRunning = false;
@@ -65,7 +218,7 @@ void renderGradientCircle(uint32_t *pixel, int x, int y, int t) {
 }
 
 void renderSinusCurve(uint32_t *pixel, int x, int y, int t) { 
-    double v = sin((double)(x + t/50) / 30.0) * 50.0;
+    double v = sinf((float)(x + t/50) / 30.0) * 50.0;
     int lineHeight = v + globalWindowHeight / 2;
     int curveWidth = 5;
     int dotWidth = 3;
@@ -102,9 +255,9 @@ void renderFlat(uint32_t* pixel, int x, int y, int t) {
 }
 
 void renderWeirdGradient2(uint32_t *pixel, int x, int y, int t) {
-    float rg = gradient(200, 200, x, y, 500000);
-    float gg = gradient(globalWindowWidth, 100, x, y, 500000);
-    float bg = gradient(globalWindowWidth/2, 500, x, y, 500000);
+    float rg = gradient(200, 200, x, y, 100000);
+    float gg = gradient(globalWindowWidth, 100, x, y, 100000);
+    float bg = gradient(globalWindowWidth/2, 500, x, y, 100000);
 
     uint8_t r =  255 * rg;
     uint8_t g = 255 * gg;
@@ -244,6 +397,26 @@ HWND setupWindowHandle(WNDCLASSA windowClass, HINSTANCE instance) {
     );
 }
 
+
+int16_t squareSoundSampler(AudioWrapper *audioWrapper) {
+    int toneHz = 256;
+    int squareWavePeriod = audioWrapper->samplesPerSecond / toneHz;
+    int halfSquareWavePeriod = squareWavePeriod / 2;
+    int16_t upTone = 1000;
+    int16_t downTone = -1000;
+
+    return ((audioWrapper->runningSampleIndex / halfSquareWavePeriod) % 2)? upTone : downTone;
+}
+
+int16_t sineSoundSampler(AudioWrapper *audioWrapper) {
+    float_t toneHz = 512;
+    float_t wavePeriod = (float) audioWrapper->samplesPerSecond / toneHz;
+    float_t t = audioWrapper->runningSampleIndex / wavePeriod;
+    float_t sineValue = sinf(t);
+    int16_t toneVolume = 3500;
+    return (int16_t)(sineValue * toneVolume);
+}
+
 int WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR commandLine, int showCode){
     setupGlobals();
 
@@ -260,9 +433,14 @@ int WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR commandLine, int s
         println("Create Window failed");
         return 0;
     }
+    HDC deviceContext = GetDC(windowHandle); // TODO: Make sure this is not an issue
+
+    // Audio setup
+    AudioWrapper audioWrapper(windowHandle, 48000, sineSoundSampler);
 
     // Main Loop
     globalIsRunning = true;
+    bool soundIsPlaying = false;
     int t = 0;
     while (globalIsRunning) {
         MSG message;
@@ -274,18 +452,10 @@ int WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR commandLine, int s
             DispatchMessageA(&message);
         }
 
-        // renderWeirdGradient(xOffset, yOffset, 60.0, 360.0);
-        // render(t, *renderGradientCircle);
-        // render(t, *renderWeirdGradient);
-        // render(t, *renderSinusCurve);
         render(t, *renderWeirdGradient2);
-        // render(t, *renderFlat);
-
-        HDC deviceContext = GetDC(windowHandle);
         updateBuffer(deviceContext);
-        ReleaseDC(windowHandle, deviceContext);
-
-        t+= 20;
+        audioWrapper.tick(t);
+        t+= 1;
     }
 
     return 0;
